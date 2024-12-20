ee8ca91c88efbcf5f47f7deaf1686d5367b7ee29
SeifEldien Galal <110824232+SeifEldienGalal@users.noreply.github.com>
2024-12-20 15:08:19 +0200

    Add files via upload

---
+++ i2c_2/i2c_2/Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Application\app.c
+
+HAL\kpd.c
+
+HAL\lcd_I2C.c
+
+HAL\servo.c
+
+MCAL\gpio.c
+
+MCAL\twi.c
+
---
+++ i2c_2/i2c_2/main.c
@@ -0,0 +1,18 @@
+/*
+ * i2c_2.c
+ *
+ * Created: 12/10/2024 11:05:37 PM
+ * Author : SeifEldien
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
